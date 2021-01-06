0a7057aa15e58a6eb3279281dc61ced7ce7405c4
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2021-01-06 21:47:24 +0100

    buszrajz

--- IRF_Project/Helyfoglalas.Designer.cs
+++ IRF_Project/Helyfoglalas.Designer.cs
@@ -29,19 +29,35 @@ namespace IRF_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1, 1);
+            this.dataGridView1.TabIndex = 0;
             // 
             // Helyfoglalas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 738);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Helyfoglalas";
             this.Text = "Helyfoglalas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- IRF_Project/Helyfoglalas.cs
+++ IRF_Project/Helyfoglalas.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
+using System.IO;
 
 namespace IRF_Project
 {
     public partial class Helyfoglalas : Form
     {
+        UtazasokEntities context = new UtazasokEntities();
         public Helyfoglalas()
         {
             InitializeComponent();
+            Adatbetoltes();
+            Ulesrajzolo();
+        }
+        private List<FoglalashozAdatok> _foglalashozadatok = new List<FoglalashozAdatok>();
+        private void Adatbetoltes() 
+        {
+            _foglalashozadatok.Clear();
+
+            int azon = 1;//int.Parse(GlobalisValtozok.Idk);
+
+            var eredmeny = from x in context.Busz
+                           join y in context.Buszules
+                           on x.BuszulesID equals y.BuszulesID
+                           join z in context.Ules
+                           on y.UlesID equals z.UlesID
+                           where x.BuszID == azon
+                           select new { z.Ulesszam, z.Pozicio, y.Foglaltsag };
+
+             _foglalashozadatok = (List<FoglalashozAdatok>)eredmeny;
+
+
+        }
+
+        private void Ulesrajzolo() 
+        {
+            int lineWidth = 5;
+
+            for (int row = 1; row < 4; row++)
+            {
+                for (int col = 1; col < 13; col++)
+                {
+                    Ulesek ules = new Ulesek();
+                    ules.Left = col * ules.Width + (int)(Math.Floor((double)(col / 4))) * lineWidth;
+                    ules.Top = row * ules.Height + (int)(Math.Floor((double)(row / 4))) * lineWidth;
+                    this.Controls.Add(ules);
+                }
+            }
         }
     }
 }
