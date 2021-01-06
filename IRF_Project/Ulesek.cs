0ea5efc3968aac9f6c595763c3f723274dd661ac
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2021-01-06 19:55:16 +0100

    Ulesek

--- IRF_Project/Ulesek.cs
+++ IRF_Project/Ulesek.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,38 @@ namespace IRF_Project
     class Ulesek : Button
     {
 
+        private int _foglaltsag;
+        public int Foglaltsag
+        {
+            get { return _foglaltsag; }
+            set
+            {
+                _foglaltsag = value;
+                if (_foglaltsag == 0)
+                    Text = "Szabad";
+                else
+                    Foglaltules();
+
+            }
+        }
+
+        public Ulesek()
+        {
+            Height = 30;
+            Width = Height;
+            BackColor = Color.White;
+            Foglaltsag = 0;
+            MouseClick += Ulesek_MouseClick;
+        }
+
+        private void Ulesek_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_foglaltsag != 0) return;
+        }
+
+        private void Foglaltules() {
+            Text = "Foglalt";
+            BackColor = Color.LightGray;
+        }
     }
 }
